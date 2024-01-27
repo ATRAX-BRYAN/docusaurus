ddf25bdb2e78ad93f0cd6a0fcd5caa01c2d0a153
ATRAX-BRYAN <157229708+ATRAX-BRYAN@users.noreply.github.com>
2024-01-27 11:37:40 -0500

    Add files via upload

---
+++ docusaurus.config.js
@@ -0,0 +1,148 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Documentación Chat Buho',
+  tagline: 'Guia oficial completa y definitiva de Chat Buho',
+  favicon: 'https://i.pinimg.com/originals/cd/36/19/cd3619f9e171f176bf0774017147170d.png',
+
+  // Set the production url of your site here
+  url: 'https://ATRAX-BRYAN.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/docusaurus/',
+
+  // GitHub pages deployment config.
+  organizationName: 'ATRAX-BRYAN',
+  projectName: 'docusaurus',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          // sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/'
+        },
+        // blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  plugins: [
+    require.resolve("@cmfcmf/docusaurus-search-local"),
+    'plugin-image-zoom',
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'https://i.pinimg.com/736x/e3/d7/12/e3d7127d82fa05afaa64b699c3ad1538.jpg',
+      navbar: {
+        title: '',
+        logo: {
+          alt: 'Documentación de Chat Buho',
+          src: 'https://i.pinimg.com/originals/cd/36/19/cd3619f9e171f176bf0774017147170d.png',
+        },
+        items: [
+          // {
+          //   type: 'docSidebar',
+          //   sidebarId: 'tutorialSidebar',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        // links: [
+        //   {
+        //     title: 'Docs',
+        //     items: [
+        //       {
+        //         label: 'Tutorial',
+        //         to: '/docs/intro',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'Community',
+        //     items: [
+        //       {
+        //         label: 'Stack Overflow',
+        //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+        //       },
+        //       {
+        //         label: 'Discord',
+        //         href: 'https://discordapp.com/invite/docusaurus',
+        //       },
+        //       {
+        //         label: 'Twitter',
+        //         href: 'https://twitter.com/docusaurus',
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: 'More',
+        //     items: [
+        //       {
+        //         label: 'Blog',
+        //         to: '/blog',
+        //       },
+        //       {
+        //         label: 'GitHub',
+        //         href: 'https://github.com/facebook/docusaurus',
+        //       },
+        //     ],
+        //   },
+        // ],
+        // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: darkCodeTheme,
+        darkTheme: darkCodeTheme,
+        additionalLanguages: ['php'],
+      },
+      theme: {
+        customCss: require.resolve('./src/css/custom.css'),
+      },
+      imageZoom: {
+        selector: '.markdown img',
+      },
+    }),
+    scripts: [
+      {
+        src: 'https://static.buho.la/fastura/documentacion.js',
+        async: true,
+      },
+    ],
+};
+
+module.exports = config;
